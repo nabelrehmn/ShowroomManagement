a96ed5421149d45618d3c6bd2b19d9ac69db520a
Nabeel Rehman <136803583+nabelrehmn@users.noreply.github.com>
2024-01-16 18:14:32 +0500

    In Progress Dashboard Set

---
+++ ShowroomManagement/ShowroomManagement/wwwroot/ProjectFiles/Department/Index.js
@@ -0,0 +1,40 @@
+﻿
+
+$(document).ready(function () {
+    LoadData()
+});
+
+// ------------- DEPARMENT DATA FATCH FROM API ------------- //
+function LoadData() {
+    $.ajax({
+        url: APIURLS.Department_GetDepartments,
+        type: 'Get',
+        data: null,
+        success: function (response) {
+            var data = JSON.parse(response)
+            for (let item of data.Response) {
+                $('#example1 tbody').append(`
+                <tr>
+                    <td>${item.Name}</td>
+                    <td>${item.Description}</td>
+                </tr>
+                `);
+            }
+
+            $("#example1").DataTable({
+                "responsive": true, "lengthChange": false, "autoWidth": false,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        }
+    });
+}
+
---
+++ ShowroomManagement/ShowroomManagement/wwwroot/ProjectFiles/Utilities/ApiUrls.js
@@ -0,0 +1,9 @@
+﻿
+
+let mail_api = 'http://localhost:5059/api';
+
+let APIURLS = {
+    // --------- DEPARTMENT URL --------- //
+    Department_GetDepartments: mail_api + '/Department/GetDepartments'
+};
+
